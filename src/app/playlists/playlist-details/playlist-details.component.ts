ac1917f77a5c47cb63eeb9382fa26e22472719f4
Artur Perwenis <artur.perwenis@tieto.com>
2018-08-06 13:11:29 +0200

    Playlist interface

--- src/app/playlists/playlist-details/playlist-details.component.ts
+++ src/app/playlists/playlist-details/playlist-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Playlist }
 
 @Component({
   selector: 'app-playlist-details',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaylistDetailsComponent implements OnInit {
 
+  playlist: Playlist = {
+    id: 123,
+    name: "Angular hits!",
+    color: "#ff0000",
+    favourite: true
+  };
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
   }
 
 }
