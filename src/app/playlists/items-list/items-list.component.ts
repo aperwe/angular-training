7d616f007370034a465ac8d5fe42b63a579013b4
Artur Perwenis <artur.perwenis@tieto.com>
2018-08-06 16:55:06 +0200

    ngFor list of songs

--- src/app/playlists/items-list/items-list.component.ts
+++ src/app/playlists/items-list/items-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Playlist } from '../../models/playlist';
 
 @Component({
   selector: 'app-items-list',
@@ -7,7 +8,32 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ItemsListComponent implements OnInit {
 
-  constructor() { }
+  playlists: Playlist[] = [
+    {
+      id: 123, name: "Powerful", color: "#ff0000", favourite: true
+    },
+    {
+      id: 124, name: "Adagio", color: "#ff0000", favourite: false
+    },
+    {
+      id: 125, name: "TOP 20", color: "#ff0000", favourite: true
+    },
+    {
+      id: 126, name: "Best of angular", color: "#ff0000", favourite: true
+    }
+  ]
+
+  trackFn(index, elem)
+  {
+    return elem.id;
+  }
+  constructor() {
+    setInterval(() => {
+      this.playlists.unshift(this.playlists.pop());
+      this.playlists.unshift(this.playlists.pop());
+      this.playlists.unshift(this.playlists.pop());
+    }, 1500)
+  }
 
   ngOnInit() {
   }
